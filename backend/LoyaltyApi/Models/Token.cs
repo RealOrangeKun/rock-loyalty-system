22a1b9dc656f4fcb8db33c2fb8cd7c53526f6444
Orangekun <amazingritro66@gmail.com>
2024-08-15 19:02:51 +0300

    added token model

---
+++ backend/LoyaltyApi/Models/Token.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoyaltyApi.Models
+{
+    public class Token
+    {
+        [Key]
+        public required string TokenValue { get; set; }
+
+        public required TokenType TokenType { get; set; }
+
+        public required int CustomerId { get; set; }
+
+        public required int RestaurantId { get; set; }
+    }
+}
