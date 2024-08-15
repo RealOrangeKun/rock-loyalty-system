7fc290bb5d0151b874e84c02791dede7f2c5a3e6
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-15 18:47:43 +0300

    added Restaurant model

---
+++ backend/LoyaltyApi/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿namespace LoyaltyApi.Models;
+
+public class Restaurant
+{
+    [Key]
+    public required int RestaurantId { get; set; }
+    
+    // Rates for Credit Points and Loyalty Points
+    public double CreditPointsBuyingRate { get; set; }
+    public double CreditPointsSellingRate { get; set; }
+    public double LoyaltyPointsBuyingRate { get; set; }
+    public double LoyaltyPointsSellingRate { get; set; }
+
+    // Lifetime values in Days for Credit Points and Loyalty Points
+    public int CreditPointsLifeTime { get; set; }
+    public int LoyaltyPointsLifeTime { get; set; }
+        
+    // Lifetime value in Milliseconds for Vouchers
+    public int VoucherLifeTime { get; set; }
+}
