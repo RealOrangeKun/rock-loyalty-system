39680521dc617734f6e06e9d056f5463f3aaf284
Orangekun <amazingritro66@gmail.com>
2024-08-16 19:08:24 +0300

    added google options class

---
+++ backend/LoyaltyApi/Config/GoogleOptions.cs
@@ -0,0 +1,8 @@
+namespace LoyaltyApi.Config
+{
+    public class GoogleOptions
+    {
+        public required string ClientId { get; set; }
+        public required string ClientSecret { get; set; }
+    }
+}
