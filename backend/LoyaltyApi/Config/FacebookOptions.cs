c29e62e8ec13077c5f7295d8f2dbe3077cce18fb
Orangekun <amazingritro66@gmail.com>
2024-08-16 19:08:12 +0300

    added facebook options class

---
+++ backend/LoyaltyApi/Config/FacebookOptions.cs
@@ -0,0 +1,8 @@
+namespace LoyaltyApi.Config
+{
+    public class FacebookOptions
+    {
+        public required string AppId { get; set; }
+        public required string AppSecret { get; set; }
+    }
+}
