6460337a771b31f96b27d0a0810170dbcbc49332
Orangekun <amazingritro66@gmail.com>
2024-08-16 19:07:29 +0300

    added oauth 2 for google and facebook

--- backend/LoyaltyApi/Controllers/AuthController.cs
+++ backend/LoyaltyApi/Controllers/AuthController.cs
@@ -1,12 +1,18 @@
+using System.Security.Claims;
+using LoyaltyApi.Config;
 using LoyaltyApi.Models;
 using LoyaltyApi.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace LoyaltyApi.Controllers
 {
     [ApiController]
     [Route("api/auth")]
-    public class AuthController(ITokenService tokenService) : ControllerBase
+    public class AuthController(ITokenService tokenService, IOptions<JwtOptions> jwtOptions, ILogger<AuthController> logger) : ControllerBase
     {
         [HttpPost]
         [Route("login")]
@@ -14,15 +20,9 @@ namespace LoyaltyApi.Controllers
         {
             // TODO: use actual userService and delete dummy user dictionary
             // var user = await userService.ValidateUserAsync(loginBody);
-            var user = new Dictionary<string, int>();
-            string accessToken = tokenService.GenerateAccessToken(user["id"], loginBody.RestaurantId);
-            string refreshToken = await tokenService.GenerateRefreshTokenAsync(user["id"], loginBody.RestaurantId);
-            HttpContext.Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict
-            });
+            string accessToken = tokenService.GenerateAccessToken(1, loginBody.RestaurantId);
+            string refreshToken = await tokenService.GenerateRefreshTokenAsync(1, loginBody.RestaurantId);
+            HttpContext.Response.Cookies.Append("refreshToken", refreshToken, jwtOptions.Value.JwtCookieOptions);
             return Ok(new { accessToken });
         }
         [HttpPost]
@@ -31,6 +31,56 @@ namespace LoyaltyApi.Controllers
         {
             // await userService.AddUserAsync(registernBody);
             return Created();
+        }
+        [HttpPost("signin-facebook")]
+        public ActionResult SignInWithFacebook([FromQuery] string restaurantID)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("FacebookCallback", new { restaurantID }) };
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+        [HttpGet("signin-facebook/callback")]
+        public async Task<ActionResult> FacebookCallback(string restaurantID)
+        {
+            var result = await HttpContext.AuthenticateAsync(FacebookDefaults.AuthenticationScheme);
+            if (!result.Succeeded) return Unauthorized();
+            var claims = result.Principal.Identities.FirstOrDefault()?.Claims;
+            var name = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            //TODO: use customer service to check if the customer already exists if not then add them
+            //TODO: find user with email and res id if found then get the cid and put it in first param
+            _ = int.TryParse(restaurantID, out var restaurantId);
+            var accessToken = tokenService.GenerateAccessToken(1, restaurantId);
+            string refreshToken = await tokenService.GenerateRefreshTokenAsync(1, restaurantId);
+            HttpContext.Response.Cookies.Append("refreshToken", refreshToken, jwtOptions.Value.JwtCookieOptions);
+            return Ok(new { accessToken });
+        }
+
+        [HttpGet("signin-google")]
+        public ActionResult SignInWithGoogle([FromQuery] string restaurantID)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleCallback", new { restaurantID }) };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("signin-google/callback")]
+        public async Task<ActionResult> GoogleCallback(string restaurantID)
+        {
+            var result = await HttpContext.AuthenticateAsync(GoogleDefaults.AuthenticationScheme);
+            if (!result.Succeeded) return Unauthorized();
+
+            var claims = result.Principal?.Identities.FirstOrDefault()?.Claims;
+            var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+            var name = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+
+            // TODO: Use customer service to check if the customer already exists, if not, add them
+            // TODO: Find user with email and restaurant ID. If found, get the customerId and use it below
+            _ = int.TryParse(restaurantID, out var restaurantId);
+
+            // Generate JWT tokens
+            var accessToken = tokenService.GenerateAccessToken(1, restaurantId);
+            string refreshToken = await tokenService.GenerateRefreshTokenAsync(1, restaurantId);
+            HttpContext.Response.Cookies.Append("refreshToken", refreshToken, jwtOptions.Value.JwtCookieOptions);
+            return Ok(new { accessToken });
         }
     }
 }
