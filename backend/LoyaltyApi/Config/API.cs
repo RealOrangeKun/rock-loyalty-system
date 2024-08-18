cea77bfa78378bb7559a6592a942b9dbca0f22a4
Orangekun <amazingritro66@gmail.com>
2024-08-18 19:35:25 +0300

    added api model

---
+++ backend/LoyaltyApi/Config/API.cs
@@ -0,0 +1,10 @@
+
+namespace LoyaltyApi.Config
+{
+    public class API
+    {
+        public required string BaseUrl { get; set; }
+        public required string UserId { get; set; }
+        public required string Password { get; set; }
+    }
+}
