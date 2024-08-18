c601d2b646ed3367c7ed51d07134532f76f9874e
Orangekun <amazingritro66@gmail.com>
2024-08-18 19:33:53 +0300

    added getuser method

--- backend/LoyaltyApi/Controllers/UsersController.cs
+++ backend/LoyaltyApi/Controllers/UsersController.cs
@@ -2,14 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LoyaltyApi.Models;
+using LoyaltyApi.Repositories;
+using LoyaltyApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoyaltyApi.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    [Route("api/user")]
+    public class UsersController(IUserService userService) : ControllerBase
     {
-        
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult> CreateUser([FromBody] User user)
+        {
+            try
+            {
+                await userService.CreateUserAsync(user);
+                return Ok("User created");
+            }
+            catch (HttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+
+
+        }
     }
 }
