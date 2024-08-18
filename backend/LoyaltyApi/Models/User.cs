d574a9d8aae84b3bac5d075857e3e66335a369df
Orangekun <amazingritro66@gmail.com>
2024-08-18 13:56:57 +0300

    added user model

---
+++ backend/LoyaltyApi/Models/User.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyaltyApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public required string Name { get; set; }
+        public int RestaurantId { get; set; }
+
+    }
+}
