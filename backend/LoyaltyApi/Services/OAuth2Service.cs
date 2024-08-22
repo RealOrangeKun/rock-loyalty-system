9aef80f01360fbe04c4b3611c93bd4e7e394b9ab
Orangekun <amazingritro66@gmail.com>
2024-08-22 15:43:08 +0300

    added adding user to database

--- backend/LoyaltyApi/Services/OAuth2Service.cs
+++ backend/LoyaltyApi/Services/OAuth2Service.cs
@@ -6,10 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LoyaltyApi.Models;
 
 namespace LoyaltyApi.Services
 {
-    public class OAuth2Service(ITokenService tokenService, IOptions<JwtOptions> jwtOptions)
+    public class OAuth2Service(ITokenService tokenService, IOptions<JwtOptions> jwtOptions, IUserService userService)
     {
         public async Task<IActionResult> HandleCallbackAsync(HttpContext context, string authenticationScheme)
         {
@@ -17,13 +18,21 @@ namespace LoyaltyApi.Services
             if (!result.Succeeded) return new UnauthorizedResult();
 
             var claims = result.Principal?.Identities.FirstOrDefault()?.Claims;
-            var name = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
-            var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
-
-            // TODO: Use customer service to check if the customer already exists, if not, add them
-            // TODO: Find user with email and restaurant ID. If found, get the customerId and use it below
-
+            string? name = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value ?? throw new ArgumentException("Name not found");
+            string? email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value ?? throw new ArgumentException("Email not found");
             var restaurantId = Convert.ToInt32(result.Properties.Items["resId"]);
+            User? user = await userService.GetAndValidateUserAsync(null, email, null, restaurantId);
+            if (user == null)
+            {
+                user = new()
+                {
+                    Email = email,
+                    Name = name,
+                    RestaurantId = restaurantId,
+                    PhoneNumber = "123"
+                };
+                await userService.CreateUserAsync(user);
+            };
             var accessToken = tokenService.GenerateAccessToken(1, restaurantId);
             var refreshToken = await tokenService.GenerateRefreshTokenAsync(1, restaurantId);
             context.Response.Cookies.Append("refreshToken", refreshToken, jwtOptions.Value.JwtCookieOptions);
