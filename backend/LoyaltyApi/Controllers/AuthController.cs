1e14759d6df2c5e391be504c774111a8ca206da5
Orangekun <amazingritro66@gmail.com>
2024-08-22 13:57:25 +0300

    changed paramter order

--- backend/LoyaltyApi/Controllers/AuthController.cs
+++ backend/LoyaltyApi/Controllers/AuthController.cs
@@ -16,7 +16,7 @@ namespace LoyaltyApi.Controllers
         public async Task<ActionResult> Login([FromBody] LoginRequestBody loginBody)
         {
 
-            User? user = await userService.GetAndValidateUserAsync(loginBody.Email, loginBody.PhoneNumber, loginBody.Password, loginBody.RestaurantId);
+            User? user = await userService.GetAndValidateUserAsync(loginBody.PhoneNumber, loginBody.Email, loginBody.Password, loginBody.RestaurantId);
             // TODO: make random id if testing enviroment
             if (user == null) return Unauthorized();
             string accessToken = tokenService.GenerateAccessToken(user.Id, loginBody.RestaurantId);
