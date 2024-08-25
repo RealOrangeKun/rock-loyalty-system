b9235a05d6d22429966eebbf781a3ae3a65348a4
Orangekun <amazingritro66@gmail.com>
2024-08-25 18:59:07 +0300

    remvoed useless depency

--- backend/LoyaltyApi/Utilities/ApiUtility.cs
+++ backend/LoyaltyApi/Utilities/ApiUtility.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Options;
 
 namespace LoyaltyApi.Utilities
 {
-    public class ApiUtility(IOptions<API> apiOptions, ILogger<ApiUtility> logger)
+    public class ApiUtility(IOptions<API> apiOptions)
     {
         public async Task<string> GetApiKey(string restaurantId)
         {
@@ -50,28 +50,62 @@ namespace LoyaltyApi.Utilities
             var result = await client.PostAsync($"{apiOptions.Value.BaseUrl}/api/HISCMD/ADDVOC", content);
             return await result.Content.ReadAsStringAsync();
         }
-        public async Task<User?> GetUserAsync(string? email, string? phoneNumber, int restaurantId, string apiKey)
+        public async Task<User?> GetUserAsync(User user, string apiKey)
         {
             using HttpClient client = new();
             client.DefaultRequestHeaders.Add("XApiKey", apiKey);
-            logger.LogCritical(email);
-            string url = $"{apiOptions.Value.BaseUrl}/api/concmd/GETCON/C/{phoneNumber ?? email}";
+            string url = $"{apiOptions.Value.BaseUrl}/api/concmd/GETCON/C/{user.PhoneNumber ?? user.Email ?? throw new ArgumentException("Phone number or email is missing")}";
             var result = await client.GetAsync(url);
             var json = await result.Content.ReadAsStringAsync();
-            logger.LogCritical(json);
-            if (json.ToString().Replace(" ", "").Contains("ERR")) return null;
+            if (json.ToString().Replace(" ", "").Contains("ERR")) throw new HttpRequestException("User not found");
             var userJson = JsonSerializer.Deserialize<JsonElement>(json);
-            User? user = new()
+            User? createdUser = new()
             {
                 Id = userJson.GetProperty("CNO").GetInt32(),                   // Mapping "CNO" to User.Id
                 PhoneNumber = userJson.GetProperty("TEL1").GetString()!,       // Mapping "TEL1" to User.PhoneNumber
                 Email = userJson.GetProperty("EMAIL").GetString()!,            // Mapping "EMAIL" to User.Email
                 Name = userJson.GetProperty("CNAME").GetString()!,             // Mapping "CNAME" to User.Name
-                RestaurantId = restaurantId,                                   // Use the passed restaurantId
+                RestaurantId = user.RestaurantId,                                   // Use the passed restaurantId
                 // Password = userJson.GetProperty("PASS").GetString()!,
             };
-            return user;
+            return createdUser;
 
+        }
+        public async Task<User?> CreateUserAsync(User user, string apiKey)
+        {
+            using HttpClient client = new();
+            var body = new
+            {
+                CCODE = "C", // Constant For Customer 
+                CNO = "0", // 0 if New Customer else Check The Cno if Exist Update Else Insert
+                CNAME = user.Name,
+                FORDES = user.Name, // foreign desc
+                // PASS = user.Password, // password
+                TEL1 = user.PhoneNumber,
+                TEL2 = "",
+                EMAIL = user.Email,
+                EMAIL1 = "",
+            };
+            // Serialize the body object to JSON
+            string jsonBody = JsonSerializer.Serialize(body);
+
+            var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var isDevelopment = string.Equals(envName, "Development", StringComparison.OrdinalIgnoreCase);
+
+            client.DefaultRequestHeaders.Add("XApiKey", apiKey);
+
+            // Create a StringContent object with the JSON and specify the content type
+            StringContent content = new(jsonBody, Encoding.UTF8, "application/json");
+
+            // Send the POST request
+            HttpResponseMessage response = await client.PostAsync($"{apiOptions.Value.BaseUrl}/api/CONCMD/ADDCON", content);
+
+            // Check the response status and handle accordingly
+            string message = await response.Content.ReadAsStringAsync();
+            if (message.Replace(" ", "").Contains("ERR"))
+                throw new HttpRequestException($"Request to create user failed with message: {message}");
+
+            return user;
         }
     }
 }
