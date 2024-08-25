57cab4e11bbfc1cf735e35adefe07eba282986b2
Orangekun <amazingritro66@gmail.com>
2024-08-25 18:58:49 +0300

    cleaned service

--- backend/LoyaltyApi/Services/OAuth2Service.cs
+++ backend/LoyaltyApi/Services/OAuth2Service.cs
@@ -1,16 +1,14 @@
 ﻿using System.Security.Claims;
 using LoyaltyApi.Config;
-using LoyaltyApi.Services;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LoyaltyApi.Models;
+using LoyaltyApi.Repositories;
 
 namespace LoyaltyApi.Services
 {
-    public class OAuth2Service(ITokenService tokenService, IOptions<JwtOptions> jwtOptions, IUserService userService)
+    public class OAuth2Service(ITokenService tokenService, IOptions<JwtOptions> jwtOptions, IUserService userService, IUserRepository userRepository)
     {
         public async Task<IActionResult> HandleCallbackAsync(HttpContext context, string authenticationScheme)
         {
@@ -21,7 +19,7 @@ namespace LoyaltyApi.Services
             string? name = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value ?? throw new ArgumentException("Name not found");
             string? email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value ?? throw new ArgumentException("Email not found");
             var restaurantId = Convert.ToInt32(result.Properties.Items["resId"]);
-            User? user = await userService.GetAndValidateUserAsync(null, email, null, restaurantId);
+            User? user = await userService.GetUserByEmailAsync(email, restaurantId);
             if (user == null)
             {
                 user = new()
@@ -31,12 +29,12 @@ namespace LoyaltyApi.Services
                     RestaurantId = restaurantId,
                     PhoneNumber = "123"
                 };
-                await userService.CreateUserAsync(user);
+                await userRepository.CreateUserAsync(user);
             };
             var accessToken = tokenService.GenerateAccessToken(1, restaurantId);
             var refreshToken = await tokenService.GenerateRefreshTokenAsync(1, restaurantId);
             context.Response.Cookies.Append("refreshToken", refreshToken, jwtOptions.Value.JwtCookieOptions);
-            return new OkObjectResult(new { accessToken });
+            return new OkObjectResult(accessToken);
         }
     }
 }
