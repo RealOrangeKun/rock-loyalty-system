d3c2a9ccc5ef8b30941b8bfa17b9438df80fc173
Orangekun <amazingritro66@gmail.com>
2024-08-25 18:58:38 +0300

    cleaned user service interface

--- backend/LoyaltyApi/Services/IUserService.cs
+++ backend/LoyaltyApi/Services/IUserService.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LoyaltyApi.Models;
+using LoyaltyApi.RequestModels;
 
 namespace LoyaltyApi.Services
 {
     public interface IUserService
     {
-        Task<User?> GetAndValidateUserAsync(string? phoneNumber, string? email, string? password, int restaurantId);
-        Task<object> CreateUserAsync(User user);
+        Task<User?> GetUserByPhonenumberAsync(string phoneNumber, int restaurantId);
+        Task<User?> CreateUserAsync(RegisterRequestBody registerRequestBody);
+        Task<User?> GetUserByEmailAsync(string email, int restaurantId);
     }
 }
