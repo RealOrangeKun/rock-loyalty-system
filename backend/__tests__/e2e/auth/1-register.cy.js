0b517b4dc615dbe4150ee459f5139f9fd59e64e2
Orangekun <amazingritro66@gmail.com>
2024-08-25 15:58:40 +0300

    made registration test

--- backend/__tests__/e2e/auth/1-register.cy.js
+++ backend/__tests__/e2e/auth/1-register.cy.js
@@ -2,12 +2,35 @@
 const { launchUrl } = require("../../config")
 
 describe('Register', () => {
-
-    beforeEach(() => {
-        cy.log(launchUrl);
-    })
-
-    it('should register new user', () => {
-        expect(true).to.equal(true)
+    it('successfull registration test', () => {
+        const name = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        const email = name + '@example.com';
+        const password = 'Password123';
+        const phoneNumber = Math.floor(Math.random() * 10 ** 12).toString().padStart(12, '0');
+        const restaurantId = 600;
+        cy.wrap(email).as('email');
+        cy.wrap(password).as('password');
+        cy.wrap(name).as('username');
+        cy.wrap(phoneNumber).as('phoneNumber');
+        const body = {
+            name,
+            email,
+            password,
+            phoneNumber,
+            restaurantId
+        };
+        cy.request({
+            method: 'POST',
+            url: `${launchUrl}/api/user/register`,
+            body,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then(response => {
+            expect(response.status).to.equal(200);
+            cy.log('Registration successfull api responded with: ');
+            cy.log(JSON.stringify(response.body));
+        })
     })
 })
