67eeddefc88b4b62a50bd5b939cb37eb91d077f2
Orangekun <amazingritro66@gmail.com>
2024-08-26 13:07:22 +0300

    removed properties

--- backend/LoyaltyApi/RequestModels/CreateVoucherRequest.cs
+++ backend/LoyaltyApi/RequestModels/CreateVoucherRequest.cs
@@ -4,7 +4,5 @@ namespace LoyaltyApi.RequestModels
     public class CreateVoucherRequest
     {
         public int Points { get; set; }
-        public int RestaurantId { get; set; }
-        public int CustomerId { get; set; }
     }
 }
