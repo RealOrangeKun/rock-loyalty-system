2d0637f2cb00cfdedd91a447f98b051b3a6b1269
Orangekun <amazingritro66@gmail.com>
2024-08-26 13:07:39 +0300

    added using http context

--- backend/LoyaltyApi/Services/VoucherService.cs
+++ backend/LoyaltyApi/Services/VoucherService.cs
@@ -1,21 +1,22 @@
+using System.Security.Claims;
 using LoyaltyApi.Models;
 using LoyaltyApi.Repositories;
 using LoyaltyApi.RequestModels;
 using LoyaltyApi.Utilities;
-using Microsoft.AspNetCore.Mvc;
 
 namespace LoyaltyApi.Services
 {
-    public class VoucherService (IVoucherRepository voucherRepository, VoucherUtility voucherUtility , IRestaurantRepository restaurantRepository): IVoucherService
+    public class VoucherService(IVoucherRepository voucherRepository, VoucherUtility voucherUtility, IRestaurantRepository restaurantRepository, IHttpContextAccessor httpContext) : IVoucherService
     {
-        public async Task<Voucher> CreateVoucherAsync([FromBody] CreateVoucherRequest voucherRequest)
+        public async Task<Voucher> CreateVoucherAsync(CreateVoucherRequest voucherRequest)
         {
-
-           int voucherValue = voucherUtility.CalculateVoucherValue(voucherRequest.Points,restaurantRepository.GetRestaurantInfo(voucherRequest.RestaurantId).Result.CreditPointsSellingRate); 
-             Voucher voucher = new()
+            int customerId = int.Parse(httpContext.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? httpContext.HttpContext.User?.FindFirstValue("sub") ?? throw new ArgumentException("Customer not found"));
+            int restaurantId = int.Parse(httpContext.HttpContext.Items["restaurantId"]?.ToString() ?? throw new ArgumentException("Restaurant not found"));
+            int voucherValue = voucherUtility.CalculateVoucherValue(voucherRequest.Points, restaurantRepository.GetRestaurantInfo(restaurantId).Result.CreditPointsSellingRate);
+            Voucher voucher = new()
             {
-                RestaurantId = voucherRequest.RestaurantId,
-                CustomerId = voucherRequest.CustomerId,
+                RestaurantId = restaurantId,
+                CustomerId = customerId,
                 Value = voucherValue
             };
             return await voucherRepository.CreateVoucherAsync(voucher);
@@ -24,7 +25,7 @@ namespace LoyaltyApi.Services
 
         public async Task<IEnumerable<Voucher>> GetUserVouchersAsync(int customerId, int restaurantId)
         {
-           return await voucherRepository.GetUserVouchersAsync(customerId, restaurantId);
+            return await voucherRepository.GetUserVouchersAsync(customerId, restaurantId);
         }
 
         public async Task<Voucher> GetVoucherAsync(int voucherId, int restaurantId)
