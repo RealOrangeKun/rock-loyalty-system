9682ca9ab5b6684e86a32e9b536d928d6df2a897
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-26 13:06:27 +0300

    rename DbContext property from Restaurant to Restaurants

--- backend/LoyaltyApi/Repositories/RestaurantRepository.cs
+++ backend/LoyaltyApi/Repositories/RestaurantRepository.cs
@@ -9,38 +9,38 @@ namespace LoyaltyApi.Repositories
     {
         public async Task CreateRestaurant(Restaurant restaurant)
         {
-            await dbContext.Restaurant.AddAsync(restaurant);            
+            await dbContext.Restaurants.AddAsync(restaurant);            
         }
 
         public async Task<Restaurant> GetRestaurantInfo(int restaurantId){
-        var restaurant = await dbContext.Restaurant.FirstOrDefaultAsync(r => r.RestaurantId == restaurantId);
+        var restaurant = await dbContext.Restaurants.FirstOrDefaultAsync(r => r.RestaurantId == restaurantId);
         return restaurant ?? throw new DataException("Restaurant not found");
         }
 
         public async Task UpdateCreditBuyingRate(Restaurant restaurant)
         {
-            dbContext.Restaurant.Attach(restaurant);
+            dbContext.Restaurants.Attach(restaurant);
             dbContext.Entry(restaurant).Property(r => r.CreditPointsBuyingRate).IsModified = true;
             await dbContext.SaveChangesAsync();
         }
 
 
         public async Task UpdateCreditPointsLifeTime(Restaurant restaurant){
-            dbContext.Restaurant.Attach(restaurant);
+            dbContext.Restaurants.Attach(restaurant);
             dbContext.Entry(restaurant).Property(r => r.CreditPointsLifeTime).IsModified = true;
             await dbContext.SaveChangesAsync();
         }
 
         public async Task UpdateCreditSellingRate(Restaurant restaurant)
         {
-            dbContext.Restaurant.Attach(restaurant);
+            dbContext.Restaurants.Attach(restaurant);
             dbContext.Entry(restaurant).Property(r => r.CreditPointsSellingRate).IsModified = true;
             await dbContext.SaveChangesAsync();
         }
 
         public async Task UpdateVoucherLifeTime(Restaurant restaurant)
         {
-            dbContext.Restaurant.Attach(restaurant);
+            dbContext.Restaurants.Attach(restaurant);
             dbContext.Entry(restaurant).Property(r => r.VoucherLifeTime).IsModified = true;
             await dbContext.SaveChangesAsync();
         }
