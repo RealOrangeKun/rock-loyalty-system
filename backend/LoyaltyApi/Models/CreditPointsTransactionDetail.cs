678103f5f148dc98f6a3e5cf4e0ad0765155f5e6
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-28 02:39:30 +0300

    Remove "required" keyword from detail id property

--- backend/LoyaltyApi/Models/CreditPointsTransactionDetail.cs
+++ backend/LoyaltyApi/Models/CreditPointsTransactionDetail.cs
@@ -2,16 +2,16 @@
 
 public class CreditPointsTransactionDetail
 {
-    public required int DetailId { get; set; }
-    
+    public int DetailId { get; set; }
+
     public required int TransactionId { get; set; }
-    
+
     public required int EarnTransactionId { get; set; }
-    
+
     public required int PointsUsed { get; set; }
 
     // Navigation properties
     public CreditPointsTransaction Transaction { get; set; }
-    
+
     public CreditPointsTransaction EarnTransaction { get; set; }
 }
