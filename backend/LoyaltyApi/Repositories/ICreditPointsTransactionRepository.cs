e4bb4e33e78f863d029f6079492dbf046ecab667
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-28 02:47:42 +0300

    Implement a repository for Credit Points Transaction

---
+++ backend/LoyaltyApi/Repositories/ICreditPointsTransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using LoyaltyApi.Models;
+
+namespace LoyaltyApi.Repositories;
+
+public interface ICreditPointsTransactionRepository
+{
+    Task<CreditPointsTransaction?> GetTransactionByIdAsync(int transactionId);
+    
+    Task<CreditPointsTransaction?> GetTransactionByReceiptIdAsync(int receiptId);
+    
+    Task<IEnumerable<CreditPointsTransaction>> GetTransactionsByCustomerAndRestaurantAsync(int customerId, int restaurantId);
+    
+    Task AddTransactionAsync(CreditPointsTransaction transaction);
+    
+    Task UpdateTransactionAsync(CreditPointsTransaction transaction);
+    
+    Task DeleteTransactionAsync(int transactionId);
+    
+    Task<int> GetCustomerPointsAsync(int customerId, int restaurantId);
+}
