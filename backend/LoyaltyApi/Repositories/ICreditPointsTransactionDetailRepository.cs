0ac67956cb9d60a65f1d6d6e99724624e2a5b64b
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-28 02:40:43 +0300

    Add an interface for Credit Points Transaction Detail Repository

---
+++ backend/LoyaltyApi/Repositories/ICreditPointsTransactionDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using LoyaltyApi.Models;
+
+namespace LoyaltyApi.Repositories;
+
+public interface ICreditPointsTransactionDetailRepository
+{
+    
+    public Task<CreditPointsTransactionDetail?> GetTransactionDetailByIdAsync(int transactionDetailId);
+    
+    public Task<CreditPointsTransactionDetail?> GetTransactionDetailByTransactionIdAsync(int transactionId);
+    
+    public Task AddTransactionDetailAsync(CreditPointsTransactionDetail transactionDetail);
+    
+    public Task UpdateTransactionDetailAsync(CreditPointsTransactionDetail transactionDetail);
+    
+    public Task DeleteTransactionDetailAsync(int transactionDetailId);
+    
+}
