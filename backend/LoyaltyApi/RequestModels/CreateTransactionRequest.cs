303fa1504707ff1c9ebdba588b24bca7d2f14cc3
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-28 02:36:45 +0300

    Add Create Transaction Request model

---
+++ backend/LoyaltyApi/RequestModels/CreateTransactionRequest.cs
@@ -0,0 +1,13 @@
+﻿using LoyaltyApi.Models;
+
+namespace LoyaltyApi.RequestModels;
+
+public class CreateTransactionRequest
+{
+    public int CustomerId { get; set; }
+    public int RestaurantId { get; set; }
+    public int ReceiptId { get; set; }
+    public double Amount { get; set; }
+    public TransactionType TransactionType { get; set; }
+    public DateTime? TransactionDate { get; set; }
+}
