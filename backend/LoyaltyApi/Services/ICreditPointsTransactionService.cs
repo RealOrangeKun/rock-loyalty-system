b954617c1c48b8f334e0a09c9013ac4010ee150a
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-08-28 03:49:42 +0300

    Add an interface for Credit Points Transaction Service

---
+++ backend/LoyaltyApi/Services/ICreditPointsTransactionService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoyaltyApi.Models;
+using LoyaltyApi.RequestModels;
+
+namespace LoyaltyApi.Services
+{
+    public interface ICreditPointsTransactionService
+    {
+        public Task<CreditPointsTransaction?> GetTransactionByIdAsync(int transactionId);
+
+        public Task<CreditPointsTransaction?> GetTransactionByReceiptIdAsync(int receiptId);
+
+        public Task<IEnumerable<CreditPointsTransaction>> GetTransactionsByCustomerAndRestaurantAsync(int customerId,
+            int restaurantId);
+
+        public Task AddTransactionAsync(CreateTransactionRequest transactionRequest);
+
+        public Task SpendPointsAsync(int customerId, int restaurantId, int points);
+        
+        public Task<int> GetCustomerPointsAsync(int customerId, int restaurantId);
+        
+        public Task<int> ExpirePointsAsync(int customerId, int restaurantId);
+    }
+}
