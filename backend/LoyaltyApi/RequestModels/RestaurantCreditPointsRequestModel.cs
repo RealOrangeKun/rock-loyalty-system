9b842758eea2fe15df73f164cbfc3247bc8ce3ae
Orangekun <amazingritro66@gmail.com>
2024-08-28 16:12:28 +0300

    made values nullable

--- backend/LoyaltyApi/RequestModels/RestaurantCreditPointsRequestModel.cs
+++ backend/LoyaltyApi/RequestModels/RestaurantCreditPointsRequestModel.cs
@@ -2,9 +2,9 @@ namespace LoyaltyApi.RequestModels
 {
     public class RestaurantCreditPointsRequestModel
     {
-        public double CreditPointsBuyingRate { get; set; }
-        public double CreditPointsSellingRate { get; set; }
-        public int CreditPointsLifeTime { get; set; }
-        public int VoucherLifeTime { get; set; }
+        public double? CreditPointsBuyingRate { get; set; }
+        public double? CreditPointsSellingRate { get; set; }
+        public int? CreditPointsLifeTime { get; set; }
+        public int? VoucherLifeTime { get; set; }
     }
 }
