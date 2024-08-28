72c269c499bc68caa9e57343eaa87e493e89f20e
Orangekun <amazingritro66@gmail.com>
2024-08-28 12:37:12 +0300

    added transaciton controller

---
+++ backend/LoyaltyApi/Controllers/TransactionsController.cs
@@ -0,0 +1,28 @@
+using LoyaltyApi.RequestModels;
+using LoyaltyApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoyaltyApi.Controllers
+{
+    [ApiController]
+    [Route("api/transactions")]
+    public class TransactionsController(ICreditPointsTransactionService pointsTransactionService) : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> CreateTransaction(CreateTransactionRequest createTransactionRequest)
+        {
+            try
+            {
+                await pointsTransactionService.AddTransactionAsync(createTransactionRequest);
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
