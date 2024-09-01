4931304a5aede650372dc8d3efa0d0e299e76402
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:52:37 +0300

    added get user by and update

--- backend/LoyaltyApi/Services/UserService.cs
+++ backend/LoyaltyApi/Services/UserService.cs
@@ -1,22 +1,28 @@
+using System.Security.Claims;
 using LoyaltyApi.Models;
 using LoyaltyApi.Repositories;
 using LoyaltyApi.RequestModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace LoyaltyApi.Services
 {
-    public class UserService(IUserRepository userRepository) : IUserService
+    public class UserService(IUserRepository userRepository,
+        IPasswordService passwordService,
+        IHttpContextAccessor httpContext) : IUserService
     {
         public async Task<User?> CreateUserAsync(RegisterRequestBody registerRequestBody)
         {
-            User user = new()
+            User? user = new()
             {
                 Name = registerRequestBody.Name,
                 Email = registerRequestBody.Email,
                 PhoneNumber = registerRequestBody.PhoneNumber,
-                Password = registerRequestBody.Password,
                 RestaurantId = registerRequestBody.RestaurantId
             };
-            return await userRepository.CreateUserAsync(user);
+            user = await userRepository.CreateUserAsync(user);
+            if (user == null) return null;
+            await passwordService.CreatePasswordAsync(user.Id, user.RestaurantId, registerRequestBody.Password);
+            return user;
         }
 
         public async Task<User?> GetUserByEmailAsync(string email, int restaurantId)
@@ -24,6 +30,18 @@ namespace LoyaltyApi.Services
             User user = new()
             {
                 Email = email,
+                RestaurantId = restaurantId
+            };
+            return await userRepository.GetUserAsync(user);
+        }
+
+        public async Task<User?> GetUserByIdAsync()
+        {
+            int userId = int.Parse(httpContext.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new ArgumentException("userId not found"));
+            int restaurantId = int.Parse(httpContext.HttpContext?.User?.FindFirst("restaurantId")?.Value ?? throw new ArgumentException("restaurantId not found"));
+            User user = new()
+            {
+                Id = userId,
                 RestaurantId = restaurantId
             };
             return await userRepository.GetUserAsync(user);
@@ -37,6 +55,21 @@ namespace LoyaltyApi.Services
                 RestaurantId = restaurantId
             };
             return await userRepository.GetUserAsync(user);
+        }
+
+        public async Task<User> UpdateUserAsync(UpdateUserRequestModel requestModel)
+        {
+            int userId = int.Parse(httpContext.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new ArgumentException("userId not found"));
+            int restaurantId = int.Parse(httpContext.HttpContext?.User?.FindFirst("restaurantId")?.Value ?? throw new ArgumentException("restaurantId not found"));
+            User user = new()
+            {
+                Name = requestModel.Name,
+                Email = requestModel.Email,
+                PhoneNumber = requestModel.PhoneNumber,
+                Id = userId,
+                RestaurantId = restaurantId
+            };
+            return await userRepository.UpdateUserAsync(user);
         }
     }
 }
