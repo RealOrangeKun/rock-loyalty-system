c64aec5d603323cdd19e70d6248e9e75e9535343
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:51:29 +0300

    added generating forgot password token and refresh tokens function

--- backend/LoyaltyApi/Services/ITokenService.cs
+++ backend/LoyaltyApi/Services/ITokenService.cs
@@ -6,6 +6,8 @@ namespace LoyaltyApi.Services
     {
         string GenerateAccessToken(int customerId, int restaurantId, Role role);
         Task<string> GenerateRefreshTokenAsync(int customerId, int restaurantId, Role role);
-        bool ValidateRefreshToken(string token);
+        Task<string> GenerateForgotPasswordTokenAsync(int customerId, int restaurantId);
+        bool ValidateRefreshToken(string? token);
+        Task<(string accessTokenValue, string refreshTokenValue)> RefreshTokensAsync();
     }
 }
