c21a5d33b8081f47f31095b3f7c8bf907b8d597e
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:54:31 +0300

    added update user request model

---
+++ backend/LoyaltyApi/RequestModels/UpdateUserRequestModel.cs
@@ -0,0 +1,11 @@
+
+
+namespace LoyaltyApi.RequestModels
+{
+    public class UpdateUserRequestModel
+    {
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
