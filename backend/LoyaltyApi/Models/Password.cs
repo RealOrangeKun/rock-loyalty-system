b7d193f96985ae21ff3561e2373bf995a9a91fef
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:54:00 +0300

    added password model

---
+++ backend/LoyaltyApi/Models/Password.cs
@@ -0,0 +1,9 @@
+namespace LoyaltyApi.Models
+{
+    public class Password
+    {
+        public required int CustomerId { get; set; }
+        public required int RestaurantId { get; set; }
+        public string? Value { get; set; }
+    }
+}
