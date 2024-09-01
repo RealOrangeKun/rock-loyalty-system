27b07d22eeccd72c09155e6d427d5b45b6cdd737
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:51:12 +0300

    made phone number and email required

--- backend/LoyaltyApi/RequestModels/RegisterRequestBody.cs
+++ backend/LoyaltyApi/RequestModels/RegisterRequestBody.cs
@@ -2,8 +2,8 @@ namespace LoyaltyApi.RequestModels
 {
     public class RegisterRequestBody
     {
-        public string? PhoneNumber { get; set; }
-        public string? Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Email { get; set; }
         public string? Password { get; set; }
         public required int RestaurantId { get; set; }
         public required string Name { get; set; }
