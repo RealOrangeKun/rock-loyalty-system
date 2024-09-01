34106d90388db0a2a5849c90d7e47a9344b1b6e1
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:52:17 +0300

    implemented geenrating forgot password  and refreshing

--- backend/LoyaltyApi/Services/TokenService.cs
+++ backend/LoyaltyApi/Services/TokenService.cs
@@ -1,9 +1,11 @@
+using System.Security.Claims;
 using LoyaltyApi.Models;
 using LoyaltyApi.Repositories;
 
 namespace LoyaltyApi.Services
 {
-    public class TokenService(ITokenRepository repository) : ITokenService
+    public class TokenService(ITokenRepository repository,
+    IHttpContextAccessor httpContext) : ITokenService
     {
         public string GenerateAccessToken(int customerId, int restaurantId, Role role)
         {
@@ -29,14 +31,50 @@ namespace LoyaltyApi.Services
             return await repository.GenerateRefreshTokenAsync(token);
         }
 
-        public bool ValidateRefreshToken(string tokenValue)
+        public bool ValidateRefreshToken(string? tokenValue)
         {
+            if (tokenValue == null) throw new ArgumentException("Refresh token cannot be null");
             Token token = new()
             {
                 TokenType = TokenType.RefreshToken,
                 TokenValue = tokenValue
             };
             return repository.ValidateRefreshToken(token);
+        }
+
+        public async Task<(string accessTokenValue, string refreshTokenValue)> RefreshTokensAsync()
+        {
+            int customerId = int.Parse(httpContext.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new ArgumentException("customerId not found"));
+            int restaurantId = int.Parse(httpContext.HttpContext?.User?.FindFirst("restaurantId")?.Value ?? throw new ArgumentException("restaurantId not found"));
+            Token refreshToken = new()
+            {
+                CustomerId = customerId,
+                RestaurantId = restaurantId,
+                TokenType = TokenType.RefreshToken,
+                Role = Role.User
+
+            };
+            Token accessToken = new()
+            {
+                CustomerId = customerId,
+                RestaurantId = restaurantId,
+                TokenType = TokenType.AccessToken,
+                Role = Role.User
+            };
+            string refreshTokenValue = await repository.GenerateRefreshTokenAsync(refreshToken);
+            string accessTokenValue = repository.GenerateAccessToken(accessToken);
+            return (accessTokenValue, refreshTokenValue);
+        }
+
+        public async Task<string> GenerateForgotPasswordTokenAsync(int customerId, int restaurantId)
+        {
+            Token token = new()
+            {
+                CustomerId = customerId,
+                RestaurantId = restaurantId,
+                TokenType = TokenType.ForgotPasswordToken
+            };
+            return await repository.GenerateForgotPasswordTokenAsync(token);
         }
     }
 }
