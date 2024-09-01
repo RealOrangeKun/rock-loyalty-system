6141f402519612459d64d5678e48c32e1ca46538
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:49:36 +0300

    removed password property

--- backend/LoyaltyApi/Models/User.cs
+++ backend/LoyaltyApi/Models/User.cs
@@ -7,7 +7,6 @@ namespace LoyaltyApi.Models
         public int Id { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
-        public string? Password { get; set; }
         public string? Name { get; set; }
         public int RestaurantId { get; set; }
 
