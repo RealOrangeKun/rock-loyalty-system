55d98cc0d440d3fa9ca5c22c68c70704a2958704
Orangekun <amazingritro66@gmail.com>
2024-09-01 16:50:56 +0300

    made password required for login request model

--- backend/LoyaltyApi/RequestModels/LoginRequestBody.cs
+++ backend/LoyaltyApi/RequestModels/LoginRequestBody.cs
@@ -4,9 +4,9 @@ namespace LoyaltyApi.RequestModels
     {
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
-        public string? Password { get; set; }
+        public required string Password { get; set; }
         public required int RestaurantId { get; set; }
 
-        
+
     }
 }
