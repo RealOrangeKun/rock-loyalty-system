84c6143b80cafaeb39021311f0b224cd7c4c89b6
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-09-08 03:58:11 +0300

    Document password controller

--- backend/LoyaltyApi/Controllers/PasswordController.cs
+++ backend/LoyaltyApi/Controllers/PasswordController.cs
@@ -4,50 +4,125 @@ using LoyaltyApi.Services;
 using LoyaltyApi.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LoyaltyApi.Controllers
-{
-    [ApiController]
-    [Route("api/password")]
-    public class PasswordController(ITokenService tokenService,
+namespace LoyaltyApi.Controllers;
+
+/// <summary>
+/// Controller responsible for handling password-related operations.
+/// </summary>
+[ApiController]
+[Route("api/password")]
+public class PasswordController(
+    ITokenService tokenService,
     EmailUtility emailUtility,
     IUserService userService,
     IPasswordService passwordService,
     ILogger<PasswordController> logger,
     TokenUtility tokenUtility) : ControllerBase
+{
+    /// <summary>
+    /// Sends a forgot password email to the user.
+    /// </summary>
+    /// <param name="requestBody">The request body containing the user's email and restaurant ID.</param>
+    /// <returns>An ActionResult indicating the result of the operation.</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /api/password
+    ///     {
+    ///         "email": "user@example.com",
+    ///         "restaurantId": "12"
+    ///     }
+    ///
+    /// Sample response:
+    ///
+    ///     {
+    ///         "success": true,
+    ///         "message": "Forgot password email sent successfully"
+    ///     }
+    /// </remarks>
+    [HttpPost]
+    public async Task<ActionResult> SendForgotPasswordEmail([FromBody] ForgotPasswordRequestBody requestBody)
     {
-        [HttpPost]
-        public async Task<ActionResult> SendForgotPasswordEmail([FromBody] ForgotPasswordRequestBody requestBody)
+        logger.LogInformation("Forgot password request for user {Email} and restaurant {RestaurantId}",
+            requestBody.Email, requestBody.RestaurantId);
+        User? user = await userService.GetUserByEmailAsync(requestBody.Email, requestBody.RestaurantId);
+        if (user == null) return NotFound(new { success = false, message = "User not found" });
+        var forgotPasswordToken = await tokenService.GenerateForgotPasswordTokenAsync(user.Id, user.RestaurantId);
+        await emailUtility.SendEmailAsync(user.Email, $"Forgot Password for {user.Name} - {user.Email}",
+            $"Your password reset link is http://localhost:5152/api/password/reset/{forgotPasswordToken}",
+            "Rock Loyalty System");
+        return Ok(new
         {
-            logger.LogInformation("Forgot password request for user {Email} and restaurant {RestaurantId}", requestBody.Email, requestBody.RestaurantId);
-            User? user = await userService.GetUserByEmailAsync(requestBody.Email, requestBody.RestaurantId);
-            if (user == null) return NotFound();
-            var forgotPasswordToken = await tokenService.GenerateForgotPasswordTokenAsync(user.Id, user.RestaurantId);
-            await emailUtility.SendEmailAsync(user.Email, $"Forgot Password for {user.Name} - {user.Email}", $"Your password reset link is http://localhost:5152/api/password/reset/{forgotPasswordToken}", "Rock Loyalty System");
-            return Ok();
-        }
-        [HttpGet]
-        [Route("reset/{token}")]
-        public ActionResult ResetPassword(string token)
-        {
-            logger.LogInformation("Reset password request for token {Token}", token);
-            if (token == null) return Unauthorized();
-            if (!tokenService.ValidateForgotPasswordToken(token)) return Unauthorized();
-            return Ok();
-        }
-        [HttpPut]
-        [Route("{token}")]
-        public async Task<ActionResult> UpdatePassword(string? token, [FromBody] UpdatePasswordRequestModel requestBody)
-        {
-            logger.LogInformation("Update password request for customer with {Token}", token);
-            if (token == null)
-            {
-                await passwordService.UpdatePasswordAsync(null, null, requestBody.Password);
-                return Ok("Password updated");
-            };
-            Token forgotPasswordToken = tokenUtility.ReadToken(token);
-            await passwordService.UpdatePasswordAsync(forgotPasswordToken.CustomerId, forgotPasswordToken.RestaurantId, requestBody.Password);
-            return Ok("Password updated");
-        }
+            success = true, message = "Forgot password email sent successfully"
+        });
     }
 
+    /// <summary>
+    /// Resets the user's password using the provided token.
+    /// </summary>
+    /// <param name="token">The token used to reset the password.</param>
+    /// <returns>An ActionResult indicating the result of the operation.</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET /api/password/reset/{token}
+    ///
+    /// Sample response:
+    ///
+    ///     {
+    ///         "success": true,
+    ///         "message": "Password reset successful"
+    ///     }
+    /// </remarks>
+    [HttpGet]
+    [Route("reset/{token}")]
+    public ActionResult ResetPassword(string token)
+    {
+        logger.LogInformation("Reset password request for token {Token}", token);
+        if (!tokenService.ValidateForgotPasswordToken(token))
+            return Unauthorized(new { success = false, message = "Invalid token" });
+        return Ok(new
+        {
+            success = true,
+            message = "Password reset successful"
+        });
+    }
+
+    /// <summary>
+    /// Updates the user's password.
+    /// </summary>
+    /// <param name="token">The token used to validate the password update request.</param>
+    /// <param name="requestBody">The request body containing the new password.</param>
+    /// <returns>An ActionResult indicating the result of the operation.</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     PUT /api/password/{token}
+    ///     {
+    ///         "password": "newpassword123"
+    ///     }
+    ///
+    /// Sample response:
+    ///
+    ///     {
+    ///         "success": true,
+    ///         "message": "Password updated"
+    ///     }
+    /// </remarks>
+    [HttpPut]
+    [Route("{token}")]
+    public async Task<ActionResult> UpdatePassword(string? token, [FromBody] UpdatePasswordRequestModel requestBody)
+    {
+        logger.LogInformation("Update password request for customer with {Token}", token);
+        if (token == null)
+        {
+            await passwordService.UpdatePasswordAsync(null, null, requestBody.Password);
+            return Ok(new { success = true, message = "Password updated" });
+        }
+
+        Token forgotPasswordToken = tokenUtility.ReadToken(token);
+        await passwordService.UpdatePasswordAsync(forgotPasswordToken.CustomerId, forgotPasswordToken.RestaurantId,
+            requestBody.Password);
+        return Ok(new { success = true, message = "Password updated" });
+    }
 }
