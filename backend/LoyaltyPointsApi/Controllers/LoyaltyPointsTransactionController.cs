f30d17c2c598761266efdf7cfe69d09510e3d3fa
DivineAge <yussufawad0852@gmail.com>
2024-09-19 22:53:48 +0300

    fixed route naming

--- backend/LoyaltyPointsApi/Controllers/LoyaltyPointsTransactionController.cs
+++ backend/LoyaltyPointsApi/Controllers/LoyaltyPointsTransactionController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LoyaltyPointsApi.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/loyalty-points")]
     public class LoyaltyPointsTransactionController(ILoyaltyPointsTransactionService service) : ControllerBase
     {
         [HttpGet]
